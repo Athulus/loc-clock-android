dc2a373e4569b40fb2e35026da3dc5a26d4bb96a
Max Redmond <athulus1991@gmail.com>
2017-12-02 02:27:44 -0500

    inital commit

---
+++ app/src/main/java/max/loc_clock/LocationReportActivity.java
@@ -0,0 +1,19 @@
+package max.loc_clock;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by max on 10/23/17.
+ */
+
+public class LocationReportActivity extends Activity {
+
+    public void locationSync(){
+
+    }
+
+
+}
